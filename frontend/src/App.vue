9f40ae5b142a7ad2be3108df7be518cbcc3517e3
DorTeri <dortayari@gmail.com>
2023-03-22 01:03:44 +0200

    Added get started to the main-table

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,7 +17,7 @@ import UserMsg from './cmps/UserMsg.vue'
 export default {
 
   created() {
-    console.log('Vue App created')
+    this.$store.dispatch({ type: "loadBoards" });
     const user = userService.getLoggedinUser()
     if (user)  store.commit({type: 'setLoggedinUser', user})
   },
