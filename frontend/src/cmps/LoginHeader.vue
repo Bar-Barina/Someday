711ae2c81a609126c6e452379ad9aa87b5e457e1
Bar Barina <itsbarbarina@gmail.com>
2023-03-21 17:25:09 +0200

    added router links

--- frontend/src/cmps/LoginHeader.vue
+++ frontend/src/cmps/LoginHeader.vue
@@ -1,8 +1,10 @@
 <template>
+    <router-link to="/">
     <header class="login-page-header">
         <img src="https://res.cloudinary.com/boaz-sunday-proj/image/upload/v1670843553/l37uhxjpyxwwaxw6ifnp.png" alt="logo">
         <span>Someday</span>
      </header>
+    </router-link>
  </template>
  
  <script>
