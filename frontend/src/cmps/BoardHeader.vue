9dd01326e31d5fc618a35538653a227f75a018d3
ofekdov <ofekofek2002@gmail.com>
2023-03-22 11:32:29 +0200

    added up board header

--- frontend/src/cmps/BoardHeader.vue
+++ frontend/src/cmps/BoardHeader.vue
@@ -1,5 +1,7 @@
 <template>
   <section class="board-header">
+    <section class="up-header flex align-center space-between">
+
     <section class="board-name flex align-center justify-center">
     <h1 class="title editable-div" contenteditable="true">
       Board header
@@ -10,6 +12,21 @@
     <div class="icon">
   <div className="icon" v-html="getSvg('headerStar')"></div>
     </div>
+    </section>
+    <section class="flex align-center justify-center">
+
+    <div class="logged-in-user flex align-center">
+      Activity
+      <img src="../assets/img/profile-icon.png" alt="">
+    </div>
+
+    <div class="invite flex align-center">
+      <div class="icon">
+      <span v-html="getSvg('invitePeople')"></span>
+      </div>
+      Invite / 1
+    </div>
+    </section>
     </section>
   </section>
 </template>
