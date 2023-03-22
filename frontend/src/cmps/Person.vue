7745add96a22eddbdab8a514b51ad98614fa0ffe
Bar Barina <itsbarbarina@gmail.com>
2023-03-22 09:25:29 +0200

    added person ui

---
+++ frontend/src/cmps/Person.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="persons-list">
+    <person-preview
+      v-for="(person, idx) in task"
+      :key="idx"
+      :person="task.person"
+      @click="updatePerson(person)"
+    />
+  </section>
+</template>
+
+<script>
+import PersonPreview from '../cmps/PersonPreview.vue'
+export default {
+  name: 'person',
+  props: {
+    task: Object,
+  },
+  data() {
+    return {
+      selectedPerson: this.task.person,
+    }
+  },
+  methods: {
+    updatePerson(person) {
+      this.selectedPerson = person
+      this.$emit('updateTask', { cmpType: 'person', data: person })
+    },
+  },
+  components: {
+    PersonPreview,
+  },
+}
+</script>
+
+<style>
+p {
+  margin: 0;
+}
+
+.persons-list {
+  display: inline-flex;
+  justify-items: center;
+  flex-direction: row;
+  background-color: lightseagreen;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.persons {
+  background-color: lightseagreen;
+}
+</style>
