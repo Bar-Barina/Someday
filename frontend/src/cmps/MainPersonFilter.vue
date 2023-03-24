d15b94f175b6b096ade42dc201574e89bc70bef9
Bar Barina <itsbarbarina@gmail.com>
2023-03-24 22:29:56 +0300

    added person filter cmp

---
+++ frontend/src/cmps/MainPersonFilter.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="person-filter-container">
+    <div>Quick person filter</div>
+    <div class="subiteams-title">Filter items and subitems by person</div>
+    <span v-for="(member, idx) in currBoard.members" :key="idx"
+    class="person-filter-img">
+      <img :src="member.url" />
+    </span>
+  </section>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {}
+  },
+  methods: {},
+  computed: {
+    currBoard() {
+      return this.$store.getters.currBoard
+    },
+  },
+}
+</script>
