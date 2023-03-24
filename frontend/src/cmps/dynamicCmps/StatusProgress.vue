10e7111e3d3da7640bfb0b05988c11e027f76357
DorTeri <dortayari@gmail.com>
2023-03-24 17:57:44 +0300

    Added progressBar

--- frontend/src/cmps/dynamicCmps/StatusProgress.vue
+++ frontend/src/cmps/dynamicCmps/StatusProgress.vue
@@ -1,26 +1,52 @@
 <template>
   <section class="status-progress cell">
-    <div v-for="(option , idx) in options" :key="idx"
-     :style="{'background-color': colors[idx]}"></div>
+    <div
+      v-for="(option, idx) in options"
+      :key="idx"
+      :style="{ 'background-color': colors[idx] , width: this.map[option]}"
+    >
+    <div v-if="map[option]"></div>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
   props: {
-    group: Object
+    group: Object,
+    label: String,
+  },
+  created() {
+    if (this.label === "StatusProgress") {
+      this.options = ["working on it", "Done", "Stuck", ""];
+      this.colors = ["#fdab3d", "#00c875", "#e2445c", "#c3c4c3"];
+    } else {
+      this.options = ["High", "Medium", "Low", "Critical"];
+      this.colors = [
+        "rgb(64, 22, 148)",
+        "rgb(85, 89, 223)",
+        "rgb(87, 155, 252)",
+        "rgb(51, 51, 51)",
+      ];
+    }
   },
   data() {
     return {
-      options: ['working on it' , 'Done' , 'Stuck' , '' ,'High' , 'Medium' , 'Low' , 'Critical'],
-      colors: ['#fdab3d' , '#00c875' , '#e2445c' , '#c3c4c3' , 'rgb(64, 22, 148)' ,
-      'rgb(85, 89, 223)', 'rgb(87, 155, 252)' , 'rgb(51, 51, 51)']
-    }
+      options: null,
+      map: {}
+    };
   },
   computed: {
     getPercent() {
-      
-    }
-  }
-}
+      this.options.map((option) => {
+        if (!this.map[option]) this.map[option] = 0;
+        this.map[option]++;
+      });
+      const total = Object.keys(this.map).length
+      this.map = Object.keys(this.map).map(count => {
+        return count / total * 100
+      })
+    },
+  },
+};
 </script>
