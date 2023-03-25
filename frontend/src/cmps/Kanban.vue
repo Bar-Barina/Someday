cec6a5459d0477d141aa6918623ee4bb691afa86
DorTeri <dortayari@gmail.com>
2023-03-25 17:25:58 +0300

    Started kanban

--- frontend/src/cmps/Kanban.vue
+++ frontend/src/cmps/Kanban.vue
@@ -1,9 +1,46 @@
 <template>
   <section class="kanban">
-    <h1>Kanban</h1>
+    <section class="kanban-content flex">
+      <Container
+        class="smooth-dnd-container horizontal"
+        orientation="horizontal"
+        group-name="group-kanban"
+        tag="div"
+        @drop="onColumnDrop($event)"
+      >
+      <Draggable v-for="(status , idx) in statuses" :key="idx">
+        <KanbanCol :status="status"/>
+      </Draggable>
+      </Container>
+    </section>
+    <section class="kanban-filter flex column"></section>
   </section>
 </template>
 
 <script>
-export default {};
+import { Container, Draggable } from "vue3-smooth-dnd";
+import { utilService } from '../services/util.service';
+import KanbanCol from './KanbanCol.vue';
+export default {
+  data() {
+    return {
+      statuses: ['Working on it' , 'Stuck' , 'Done' , 'Blank']
+    };
+  },
+  methods: {
+    onColumnDrop(dropResult) {
+      this.statuses = utilService.applyDrag(this.statuses, dropResult)
+    },
+  },
+  computed: {
+    currBoard() {
+      return this.$store.getters.currBoard
+    },
+  },
+  components: {
+    Container,
+    Draggable,
+    KanbanCol
+  },
+};
 </script>
---
+++ frontend/src/cmps/KanbanCol.vue
@@ -0,0 +1,67 @@
+<template>
+  <section class="kanban-col" style="{'background-color': 'red'}">
+    <div class="col-header flex space-between">
+      <div>{{ status }}</div>
+    </div>
+    <section class="col-content flex column">
+      <section class="cards-container">
+        <Container
+          class="smooth-dnd-container"
+          orientation="vertical"
+          group-name="tasks"
+          tag="div"
+          :shouldAcceptDrop="(e, payload) => e.groupName === 'tasks'"
+          :get-child-payload="getColPayload()"
+          @drop="onTaskDrop(null, $event)"
+        >
+          <Draggable v-for="(task, idx) in tasks" :key="idx">
+            <KanbanCard :task="task" />
+          </Draggable>
+        </Container>
+      </section>
+    </section>
+  </section>
+</template>
+
+<script>
+import { Container, Draggable } from "vue3-smooth-dnd";
+import KanbanCard from "./KanbanCard.vue";
+export default {
+  props: {
+    status: String,
+  },
+  data() {
+    return {
+      board: this.$store.getters.currBoard,
+    };
+  },
+  methods: {
+    getColPayload(id) {
+      return (index) => {
+        return this.board.groups.filter((g) => g._id === groupId)[0].tasks[
+          index
+        ];
+      };
+    },
+    onTaskDrop() {},
+  },
+  computed: {
+    tasks() {
+      const board = JSON.parse(JSON.stringify(this.board));
+      const filteredGroups = board.groups.map((group) => {
+        return group.tasks.filter((task) => task.status === this.status);
+      });
+      const tasks = filteredGroups.reduce((acc, group) => {
+        acc = [...acc, ...group];
+        return acc;
+      }, []);
+      return tasks;
+    },
+  },
+  components: {
+    KanbanCard,
+    Container,
+    Draggable,
+  },
+};
+</script>
