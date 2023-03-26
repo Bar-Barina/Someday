fbe8e8f5a4e6b23e47059311e168f91b3f901676
DorTeri <dortayari@gmail.com>
2023-03-27 00:33:29 +0300

    Added colors to selected in 10 min

--- frontend/src/cmps/MainTable.vue
+++ frontend/src/cmps/MainTable.vue
@@ -29,6 +29,7 @@
     <Menu
       v-if="isSelected > 0"
       :selectedTasks="selectedTasks"
+      :groupColors="groupColors"
       @clearSelected="clearSelected"
     />
     <div class="Add-new-group flex align-center" @click="addGroup">
@@ -36,93 +37,100 @@
       <button>Add new group</button>
     </div>
   </Container>
-  <NoResultsFound v-if="currBoard.groups.length === 0"  @clearSearch="clearSearch"/>
+  <NoResultsFound
+    v-if="currBoard.groups.length === 0"
+    @clearSearch="clearSearch"
+  />
 </template>
 
 <script>
-import { Container, Draggable } from 'vue3-smooth-dnd'
-import GroupPreview from './GroupPreview.vue'
-import { utilService } from '../services/util.service'
-import Menu from '../cmps/Menu.vue'
-import { svgService } from '../services/svg.service.js'
-import { eventBus } from '../services/event-bus.service'
-import GroupDrag from './GroupDrag.vue'
-import NoResultsFound from './NoResultsFound.vue'
+import { Container, Draggable } from "vue3-smooth-dnd";
+import GroupPreview from "./GroupPreview.vue";
+import { utilService } from "../services/util.service";
+import Menu from "../cmps/Menu.vue";
+import { svgService } from "../services/svg.service.js";
+import { eventBus } from "../services/event-bus.service";
+import GroupDrag from "./GroupDrag.vue";
+import NoResultsFound from "./NoResultsFound.vue";
 
 export default {
-  emits: ['labelDrop'],
+  emits: ["labelDrop"],
   data() {
     return {
       board: null,
       groups: null,
       labelsOrder: [
-        'Date',
-        'Text',
-        'Priority',
-        'Person',
-        'Files',
-        'Status',
-        'Timeline',
+        "Date",
+        "Text",
+        "Priority",
+        "Person",
+        "Files",
+        "Status",
+        "Timeline",
       ],
       selectedTasks: {},
+      groupColors: [],
       newBoard: this.currBoard,
-    }
+    };
   },
   methods: {
     onGroupDrop(dropResult) {
-      const board = JSON.parse(JSON.stringify(this.currBoard))
-      let groups = board.groups
-      groups = utilService.applyDrag(groups, dropResult)
-      board.groups = groups
-      this.$store.dispatch({ type: 'updateBoard', board })
+      const board = JSON.parse(JSON.stringify(this.currBoard));
+      let groups = board.groups;
+      groups = utilService.applyDrag(groups, dropResult);
+      board.groups = groups;
+      this.$store.dispatch({ type: "updateBoard", board });
     },
     labelDrop(dropResult) {
-      let scene = [...this.labelsOrder]
-      scene = utilService.applyDrag(scene, dropResult)
-      this.labelsOrder = scene
+      let scene = [...this.labelsOrder];
+      scene = utilService.applyDrag(scene, dropResult);
+      this.labelsOrder = scene;
     },
     addSelected({ group, task }) {
-      if (!this.selectedTasks[group._id]) this.selectedTasks[group._id] = []
-      this.selectedTasks[group._id].push(task)
+      if (!this.selectedTasks[group._id]) this.selectedTasks[group._id] = [];
+        this.selectedTasks[group._id].push(task);
+        this.groupColors.push(group.color);
     },
     removeSelected({ group, taskId }) {
       const taskIdx = this.selectedTasks[group._id].findIndex(
         (t) => t.id === taskId
-      )
-      this.selectedTasks[group._id].splice(taskIdx, 1)
+      );
+      this.selectedTasks[group._id].splice(taskIdx, 1);
+      const colorIdx = this.groupColors.findIndex((c) => c === group.color);
+      this.groupColors.splice(colorIdx, 1);
       if (this.selectedTasks[group._id].length === 0)
-        delete this.selectedTasks[group._id]
+        delete this.selectedTasks[group._id];
     },
     clearSelected() {
-      this.selectedTasks = {}
+      this.selectedTasks = {};
     },
     getSvg(iconName) {
-      return svgService.getSvg(iconName)
+      return svgService.getSvg(iconName);
     },
     addGroup() {
       const newGroup = {
-        title: 'Full Stack',
-        color: '#e2445c',
+        title: "Full Stack",
+        color: "#e2445c",
         tasks: [],
-      }
-      this.$store.dispatch({ type: 'saveTask', toUpdate: { group: newGroup } })
+      };
+      this.$store.dispatch({ type: "saveTask", toUpdate: { group: newGroup } });
     },
     updateDragGroup(group) {
-      const board = this.currBoard
-      const groupIdx = board.groups.findIndex((g) => g._id === group._id)
-      board.groups.splice(groupIdx, 1, group)
-      this.$store.dispatch({ type: 'updateBoard', board })
+      const board = this.currBoard;
+      const groupIdx = board.groups.findIndex((g) => g._id === group._id);
+      board.groups.splice(groupIdx, 1, group);
+      this.$store.dispatch({ type: "updateBoard", board });
     },
     clearSearch() {
-      eventBus.emit('clearSearch')
-    }
+      eventBus.emit("clearSearch");
+    },
   },
   computed: {
     currBoard() {
-      return JSON.parse(JSON.stringify(this.$store.getters.currBoard))
+      return JSON.parse(JSON.stringify(this.$store.getters.currBoard));
     },
     isSelected() {
-      return Object.keys(this.selectedTasks).length
+      return Object.keys(this.selectedTasks).length;
     },
   },
   components: {
@@ -133,5 +141,5 @@ export default {
     GroupDrag,
     NoResultsFound,
   },
-}
+};
 </script>
