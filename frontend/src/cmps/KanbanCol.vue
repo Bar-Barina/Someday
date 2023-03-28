320d0f89a32e70716b80135b086cdff715435222
Bar Barina <itsbarbarina@gmail.com>
2023-03-28 11:21:04 +0300

    fixed workspace arrow direction in hover

--- frontend/src/cmps/KanbanCol.vue
+++ frontend/src/cmps/KanbanCol.vue
@@ -24,11 +24,11 @@
 </template>
 
 <script>
-import { Container, Draggable } from "vue3-smooth-dnd";
-import { utilService } from "../services/util.service";
-import KanbanCard from "./KanbanCard.vue";
+import { Container, Draggable } from 'vue3-smooth-dnd'
+import { utilService } from '../services/util.service'
+import KanbanCard from './KanbanCard.vue'
 export default {
-  emits: ["addStatusesMap", "removeStatusesMap"],
+  emits: ['addStatusesMap', 'removeStatusesMap'],
   props: {
     status: String,
     tasks: Array,
@@ -37,48 +37,52 @@ export default {
   data() {
     return {
       board: this.$store.getters.currBoard,
-    };
+    }
   },
   created() {},
   methods: {
     getColPayload(id) {
       return (index) => {
-        return this.tasks[index];
-      };
+        return this.tasks[index]
+      }
     },
     onTaskDrop(dropResult) {
-      if (dropResult.removedIndex === null && dropResult.addedIndex === null) return;
+      if (dropResult.removedIndex === null && dropResult.addedIndex === null)
+        return
       console.log('dropResult', dropResult)
-      const task = JSON.parse(JSON.stringify(dropResult.payload));
-      const taskIdx = this.tasks.findIndex((t) => t.id === task.id);
-      const board = JSON.parse(JSON.stringify(this.currBoard));
+      const task = JSON.parse(JSON.stringify(dropResult.payload))
+      const taskIdx = this.tasks.findIndex((t) => t.id === task.id)
+      const board = JSON.parse(JSON.stringify(this.currBoard))
       if (dropResult.addedIndex !== null && dropResult.removedIndex === null) {
         const group = board.groups.find((group) => {
-          return group.tasks.filter((t) => t.id === task.id)[0];
-        });
-        task.status = this.status;
-        this.$emit("addStatusesMap", { task, status: this.status });
-        const toUpdate = { group, task };
-        this.$store.dispatch({ type: "saveTask", toUpdate });
-      } else if (dropResult.addedIndex === null && dropResult.removedIndex !== null) {
-        this.$emit("removeStatusesMap", { taskIdx, status: this.status });
+          return group.tasks.filter((t) => t.id === task.id)[0]
+        })
+        task.status = this.status
+        this.$emit('addStatusesMap', { task, status: this.status })
+        const toUpdate = { group, task }
+        this.$store.dispatch({ type: 'saveTask', toUpdate })
+      } else if (
+        dropResult.addedIndex === null &&
+        dropResult.removedIndex !== null
+      ) {
+        this.$emit('removeStatusesMap', { taskIdx, status: this.status })
       } else {
-        this.$emit('updateMapOrder' , {status: this.status , dropResult})
+        this.$emit('updateMapOrder', { status: this.status, dropResult })
       }
     },
   },
   computed: {
     currBoard() {
-      return this.$store.getters.currBoard;
+      return this.$store.getters.currBoard
     },
     tasksCount() {
       return this.tasks.length
-    }
+    },
   },
   components: {
     KanbanCard,
     Container,
     Draggable,
   },
-};
+}
 </script>
