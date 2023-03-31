c99a715d6fd3701fcf072bddee52f3cb14acafe2
Bar Barina <itsbarbarina@gmail.com>
2023-03-31 23:58:39 +0300

    fixed description

--- frontend/src/cmps/BoardDesc.vue
+++ frontend/src/cmps/BoardDesc.vue
@@ -1,11 +1,12 @@
 <template>
-  <section class="board-desc-container"  >
+  <section class="board-desc-container">
     <section class="dialog-editable-wrapper">
-      <h1 contenteditable="true" ref="boardTitle" @focusout="updateBoard">{{ currBoard.title }}</h1>
+      <h1 contenteditable="true" ref="boardTitle" @focusout="updateBoard">
+        {{ currBoard.title }}
+      </h1>
       <div contenteditable="true" ref="boardDesc" @focusout="updateBoard">
-        {{currBoard.description}}
+        {{ currBoard.description }}
       </div>
-      <span class="feedback">Give feedback</span>
     </section>
     <!-- INFO -->
     <section class="board-info">
@@ -13,36 +14,50 @@
       <div class="board-info-title">Board info</div>
       <!-- WORKSPACE -->
       <div>
-      <div class="info-title">Workspace</div>
-      <div class="workspace-info-div flex align-center">
-        <p class="pink-div flex">
-          <span class="Main-letter">M</span>
-          <span class="home-svg" v-html="getSvg('home')"></span>
-        </p>
-        <span class="info-subtitle">Main workspace</span>
+        <div class="info-title">Workspace</div>
+        <div class="workspace-info-div flex align-center">
+          <p class="pink-div flex">
+            <span class="Main-letter">M</span>
+            <span class="home-svg" v-html="getSvg('home')"></span>
+          </p>
+          <span class="info-subtitle">Main workspace</span>
+        </div>
       </div>
-    </div>
       <!-- CREATED AT -->
       <div class="info-title">Created by</div>
       <div class="created-info-div flex align-center">
-        <img :src="currBoard.members[1].imgUrl" />
+        <img
+          :src="
+            currUser
+              ? currUser.imgUrl
+              : 'https://cdn1.monday.com/dapulse_default_photo.png'
+          "
+        />
         <span class="info-subtitle"> March 22,2023</span>
       </div>
-       <!-- OWNERS -->
-       <div class="info-title">Owners</div>
-       <div class="owners-info-div flex align-center">
-        <img :src="currBoard.members[1].imgUrl" />
-        <span class="info-subtitle"> {{currBoard.members[1].name}}</span>
+      <!-- OWNERS -->
+      <div class="info-title">Owners</div>
+      <div class="owners-info-div flex align-center">
+        <img
+          :src="
+            currUser
+              ? currUser.imgUrl
+              : 'https://cdn1.monday.com/dapulse_default_photo.png'
+          "
+        />
+        <span class="info-subtitle"> {{ currBoard.members[0].name ? currBoard.members[0].name : ' Guest' }}</span>
       </div>
       <!-- BOARD TYPE -->
       <div class="type-info-div">
         <div class="info-title">Board type</div>
         <div class="board-type flex">
-        <span v-html="getSvg('board')" class="workspace-icon"></span>
-        <span>This board is public, visible to all
-            <br>
-             team members.</span>
-     </div>
+          <span v-html="getSvg('board')" class="workspace-icon"></span>
+          <span
+            >This board is public, visible to all
+            <br />
+            team members.</span
+          >
+        </div>
       </div>
     </section>
   </section>
@@ -53,8 +68,7 @@ import { svgService } from '../services/svg.service.js'
 export default {
   name: '',
   data() {
-    return {
-    }
+    return {}
   },
   methods: {
     getSvg(iconName) {
@@ -62,18 +76,21 @@ export default {
     },
     closeDesc() {
       this.$emit('closeDesc')
-      this.$store.commit({type:'closeBlackScreen'})
+      this.$store.commit({ type: 'closeBlackScreen' })
     },
     updateBoard() {
       const board = JSON.parse(JSON.stringify(this.currBoard))
       board.description = this.$refs.boardDesc.innerText
       board.title = this.$refs.boardTitle.innerText
       this.$store.dispatch({ type: 'updateBoard', board })
-    }
+    },
   },
   computed: {
     currBoard() {
       return this.$store.getters.currBoard
+    },
+    loggedInUser() {
+      return this.$store.getters.loggedInUser
     },
   },
   created() {},
