ec5c7e2553e8816461e90090ab6244e79a895997
ofekdov <ofekofek2002@gmail.com>
2023-04-01 16:59:41 +0300

    commit

--- frontend/src/cmps/DashBoard.vue
+++ frontend/src/cmps/DashBoard.vue
@@ -114,11 +114,12 @@ export default {
       },   
       options2: {
         plugins: {
-          legend: { position: "left",
-          labels:{
+          legend: { 
+            position: "left",
+            labels:{
               usePointStyle: true
             }
-        }
+           } 
         },
       },  
     }
