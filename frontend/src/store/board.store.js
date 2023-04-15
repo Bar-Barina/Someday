9bf2077d1424fc6e845de4438754fc945dbc6219
DorTeri <dortayari@gmail.com>
2023-04-15 17:45:07 +0300

    Fixed hover on mobile

--- frontend/src/store/board.store.js
+++ frontend/src/store/board.store.js
@@ -194,7 +194,6 @@ export const boardStore = {
       try {
         const user = userService.getLoggedInUser()
         let boards = await boardService.query(user)
-        console.log(boards)
         const board = JSON.parse(JSON.stringify(boards[0]))
         if(!user && !utilService.loadFromStorage('userId')) {
           board.owner = utilService.makeId()
